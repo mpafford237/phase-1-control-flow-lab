d1b773963e4625e5d335151031154d7627cd83cb
masonPafford <mdpafford2015@gmail.com>
2024-01-11 16:21:34 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let ride = 199
+let tip = 20
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  let response;
+  
+  if (ride <= 400) {
+    response = "This one is on me!";
+  } else if (ride >= 2500) {
+    response = "No can do.";
+  } else if (ride >= 2000) {
+    response = "I will gladly take your thirty bucks.";
+  } else if (ride > 400) {
+    response = "That will be twenty bucks.";
+  }
+  return response;
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let response;
+  city === "NYC" ? response = "Ok, sounds good." : response = "No go.";
+  return response
+};
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye.";
+  }
+  return response
+};
